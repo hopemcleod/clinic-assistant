a8be7ed24a8d3cc6bdfa39cd8598327cfce3018b
hopemcleod <hope-dev@outlook.com>
2024-04-15 22:44:45 +0100

    Adds basic form to do user search/show user details.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import './App.css'
+import { Box, TextField, Typography } from '@mui/material'
 
-function App() {
+const App = () => {
   return (
-    <React.StrictMode>
-      <p data-testid="hello">Hello World!</p>
-    </React.StrictMode>
+    <Box>
+      {/*first and last name search fields */}
+      <Typography variant="h6">Search</Typography>
+      <Box border='solid' borderColor='darkgrey'>
+        <TextField id="first_name" label="First Name" />
+        <TextField id="last_name" label="Last Name" />
+      </Box>
+
+
+      {/*User's details: 
+      First Name, 
+      Last Name, 
+      DOB, 
+      Address, 
+      Gender, 
+      Date registered with clinic */}
+      <Box marginTop='50px' border='solid' borderColor='darkgrey' display='flex' flexDirection='column'>
+        <TextField id="first_name" label="First Name" disabled />
+        <TextField id="last_name" label="Last Name" disabled />
+        <TextField id="dob" label="DOB" disabled />
+        <TextField id="address" label="Address" disabled />
+        <TextField id="gender" label="Gender" disabled />
+        <TextField id="date_registered" label="Date Registered" disabled />
+      </Box>
+
+      {/*Summary textfield. */}
+      <Box marginTop='50px' border='solid' borderColor='darkgrey' display='flex' flexDirection='column'>
+        <TextField id="summary" label="Summary" disabled />
+      </Box>
+    </Box>
   )
 }
 
